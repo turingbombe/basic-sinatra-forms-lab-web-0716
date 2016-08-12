a2ea069cd64657728939b6975c1860bc47f8bc3a
Charlie Cook <turingbombe@gmail.com>
2016-08-12 09:30:58 -0400

    Basic form created

--- app.rb
+++ app.rb
@@ -2,5 +2,12 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @print_converter = {name: "Team Name", coach: "Coach", pg: "Point Guard", sg: "Shooting Guard", pf: "Power Forward", sf: "Small Forward", c: "Center"}
+    erb :team
+  end
 end
